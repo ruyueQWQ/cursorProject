5345f9323fbcb61d760128d3e31ee85e6b8cc86d
kai <kai@jiemogame.com>
2025-11-26 15:02:42 +0800

    增加端口

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,9 +5,10 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
+    port: 4000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://127.0.0.1:8080',
         changeOrigin: true
       }
     }
